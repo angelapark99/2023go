d5e6ca4143bebbbdf23e8e3a5ffd6bd8232de6be
angelapark99 <se22099@gmail.com>
2023-11-06 10:39:18 +0900

    d

---
+++ week11/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var primes [3]int
+	// primes[0] = 1
+	primes := [3]int{2, 3, 5}
+	primes[2] = 6
+
+	for i := 0; i < 3; i++ {
+		fmt.Println(primes[i])
+	}
+}
